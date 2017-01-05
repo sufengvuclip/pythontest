943e50ea4d18f14bf645d5ef25cd526001cd9975
sufeng <sufeng@vuclip.cn>
2017-01-05 18:29:51 +0800

    calculate  the most frequency words.

--- nltk/nltktest.py
+++ nltk/nltktest.py
@@ -2,7 +2,10 @@ import nltk
 import io
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
+
+# ps = PorterStemmer()
 with io.open(u"E:\\workspace\\sufeng\\pythontest\\doc\\new_year_videos.txt",'r',-1,encoding="utf-8") as f:
     str = f.read()
 
@@ -11,5 +14,8 @@ word_tokens = word_tokenize(str)
 
 filtered_sentence = [w for w in word_tokens if not w in stop_words]
 
-print(filtered_sentence)
+# stemmed_sentence = []
+# for w in filtered_sentence:
+#     stemmed_sentence.append(ps.stem(w))
+# print(stemmed_sentence)
 
